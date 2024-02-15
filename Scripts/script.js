4f886a576a92d862323bb6297ab8af2b9a0f7747
P2201267 <loric.audin@etu.univ-lyon1.fr>
2024-02-15 21:57:39 +0100

    V0.1 Ajout des technologies et d'une petite présentation + animations

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,5 +1,14 @@
 function ouvrirMenuNav(e) {
     e.classList.toggle("ouvert");
     let nav = document.querySelector("nav");
-    nav.classList.toggle("nav-ouvert");
+    let resultat = nav.classList.toggle("nav-ouvert");
+    let header = document.querySelector("header");
+    if(resultat){
+        header.classList.add("header-ouvert");
+        header.classList.remove("header-ferme");
+    }
+    else{
+        header.classList.add("header-ferme");
+        header.classList.remove("header-ouvert");
+    }
 }

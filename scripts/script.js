73804a48c9b6e095c844eadbc223814580e51775
Loric Audin <loric.audin@etu.univ-lyon1.fr>
2024-02-17 17:40:31 +0100

    V0.2 Ajout des compétences (pas encore d'images)

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,8 @@
-function ouvrirMenuNav(e) {
-    e.classList.toggle("ouvert");
+var boutonNav = document.getElementById("bouton-menu-nav");
+boutonNav.addEventListener("click", ouvrirFermerMenuNav);
+
+function ouvrirFermerMenuNav() {
+    boutonNav.classList.toggle("ouvert");
     let nav = document.querySelector("nav");
     let resultat = nav.classList.toggle("nav-ouvert");
     let header = document.querySelector("header");
@@ -12,3 +15,11 @@ function ouvrirMenuNav(e) {
         header.classList.remove("header-ouvert");
     }
 }
+
+let body = document.querySelector("body");
+body.addEventListener("click", function(e){
+    let nav = document.querySelector("nav");
+    if(nav.classList.contains("nav-ouvert") && e.target.tagName.toLowerCase() == "a"){
+        setTimeout('ouvrirFermerMenuNav()', 1000);
+    }
+})

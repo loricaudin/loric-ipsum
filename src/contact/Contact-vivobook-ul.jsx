6a9dba6eeb81213a81adb9796d393ff48eeb5e0a
Loric Audin <loricaudin@yahoo.fr>
2025-08-17 22:53:52 +0200

    V1.5.3 3ème tentative correction images Versel + Correction responsive petit écran

---
+++ src/contact/Contact-vivobook-ul.jsx
@@ -0,0 +1,13 @@
+export default function Contact() {
+    return <section id="contact">
+        <div>
+            <h2>Vous voulez me contactez ?</h2>
+            <p>Toutes mes infos se trouvent ici :</p>
+            <ul>
+                <li>Adresse mail : <a href="mailto:loricaudin@yahoo.fr">loricaudin@yahoo.fr</a></li>
+                <li>Compte LinkedIn : <a href="https://www.linkedin.com/in/loric-audin-1a1b29250/">https://www.linkedin.com/in/loric-audin-1a1b29250/</a></li>
+                <li>Numéro de téléphone : <a href="tel:+33644764441">06 44 76 44 41</a></li>
+            </ul>
+        </div>
+    </section>
+}
---
+++ src/projets/Projet-vivobook-ul.jsx
@@ -0,0 +1,106 @@
+import { useState, useRef, useEffect } from 'react';
+import './Projet.scss';
+
+export default function Projet({ nomProjet, icone, anneeCreation, type, description, lienTelechargement, lienSite, lienCodeSource, lienCodeSourceFront, lienCodeSourceBack, listeImages, listeCompetences, listeTechnologies }) {
+    const [indexDiaporama, setIndexDiaporama] = useState(0);
+    const [imageEnCoursDeChangement, setImageEnCoursDeChangement] = useState(false);
+    const diaporamaRef = useRef(null);
+    const intervalRef = useRef(null);
+
+    const afficherImageDiaporama = (positionPrecedente, nouvellePosition) => {
+        const nbImages = listeImages.length;
+        let index = nouvellePosition;
+
+        if (positionPrecedente != nouvellePosition) {
+            if (index >= nbImages) {
+                index = 0;
+            }
+            if (index < 0) {
+                index = nbImages - 1;
+            }
+
+            let positionActuelle = positionPrecedente;
+            let n = 5;
+
+            clearInterval(intervalRef.current);
+            intervalRef.current = setInterval(() => {
+                const distance = index - positionPrecedente;
+                positionActuelle += distance / n;
+                n += n / 4;
+
+                const seuil = 0.01;
+                const atteint = (distance > 0 && positionActuelle >= index - seuil) || (distance < 0 && positionActuelle <= index + seuil);
+
+                if (atteint) {
+                    positionActuelle = index;
+                    clearInterval(intervalRef.current);
+                    setImageEnCoursDeChangement(false);
+                }
+
+                const largeur = document.documentElement.clientWidth;
+                const translateX = largeur >= 700 ? - positionActuelle * 30 : - positionActuelle * 70;
+
+                if (diaporamaRef.current) {
+                    diaporamaRef.current.style.transform = `translateX(${translateX}vw)`;
+                }
+            }, 10);
+
+            setIndexDiaporama(index);
+        } else {
+            setImageEnCoursDeChangement(false);
+        }
+    }
+
+    const deplacerImageDiaporama = (n) => {
+        if (!imageEnCoursDeChangement) {
+            setImageEnCoursDeChangement(true);
+            afficherImageDiaporama(indexDiaporama, indexDiaporama + n);
+        }
+    };
+
+    const changerImageDiaporama = (n) => {
+        if (!imageEnCoursDeChangement) {
+            setImageEnCoursDeChangement(true);
+            afficherImageDiaporama(indexDiaporama, n);
+        }
+    };
+
+    return (
+        <div className="projet">
+            <div>
+                <h1>{nomProjet}</h1>
+                <h2>Année de création : {anneeCreation}</h2>
+                <p>{description}</p>
+                <div>
+                    {lienTelechargement && <a href={lienTelechargement} target="_blank"><button>Télécharger l'application</button></a>}
+                    {lienSite && <a href={lienSite} target="_blank"><button>Visiter le site</button></a>}
+                    {lienCodeSource && <a href={lienCodeSource} target="_blank"><button>Voir le code source</button></a>}
+                </div>
+            </div>
+
+            {listeImages && (
+                <div>
+                    <div className="diaporamaRealisations">
+                        <div className="imageRealisation" ref={diaporamaRef}>
+                            {listeImages.map((image, index) => (
+                                <div className="image" key={index}><img src={`src/assets/images/projets/${image}.png`}/></div>
+                            ))}
+                        </div>
+                        <a className="imageRealisationPrecedente" onClick={() => deplacerImageDiaporama(-1)}>❮</a>
+                        <a className="imageRealisationSuivante" onClick={() => deplacerImageDiaporama(1)}>❯</a>
+                    </div>
+                    <br />
+                    <div className="diaporamaListePoints">
+                        {listeImages.map((_, index) => (
+                            <span
+                                key={index}
+                                className={`lienImageRealisation${index === indexDiaporama ? ' imageRealisationActive' : ''}`}
+                                onClick={() => changerImageDiaporama(index)}
+                            ></span>
+                        ))}
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
